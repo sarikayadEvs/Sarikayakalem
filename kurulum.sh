76f6124193f4621b63716cbc2edbffa4a811b043
sarikayadEvs <140356019+sarikayadEvs@users.noreply.github.com>
2025-01-18 04:54:09 +0300

    Add files via upload

---
+++ kurulum.sh
@@ -0,0 +1,16 @@
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install -y software-properties-common wget
+
+
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
+
+
+sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/debian/ bookworm main'
+
+sudo apt update
+sudo apt install -y --install-recommends winehq-stable
+
+
+
